069e2a7cbb0bc8daed997deda3f21d9971a73d71
Martin Bruse <zondolfin@gmail.com>
2014-12-13 21:33:40 +0100

    refactor, and merging changes instead of clearing caches

--- futon.go
+++ futon.go
@@ -48,6 +48,11 @@ func init() {
 	}
 }
 
+type childFetch struct {
+	file *drive.File
+	err  error
+}
+
 // the common node functions
 type node interface {
 	fs.Node
@@ -142,238 +147,30 @@ type dir struct {
 	childrenLock sync.RWMutex
 }
 
-// file is a node that has a download url
-type file struct {
-	node
-	downloadUrl string
-}
-
-// futon is saved in $HOME/.futon/auth, but its really just the Token that is saved
-type futon struct {
-	auth         string
-	account      string
-	drive        *drive.Service
-	mountpoint   string
-	rootFolderId string
-	lastChange   int64
-	unmounted    int32
-	conn         *fuse.Conn
-	client       *http.Client
-
-	folderCacheById   map[string]*dir // used when cleaning up folders that are subjected to change
-	folderCacheByPath map[string]*dir // used when finding folders by path
-	folderCacheLock   sync.RWMutex
-
-	Token *oauth.Token
-}
-
-func (self *futon) changeList() (result []*drive.Change, err error) {
-	var changeLoader func(string) error
-	newLastChange := int64(0)
-	changeLoader = func(pageToken string) (err error) {
-		call := self.drive.Changes.List().StartChangeId(self.lastChange).IncludeDeleted(true)
-		if pageToken != "" {
-			call = call.PageToken(pageToken)
-		}
-		changes, err := call.Do()
-		if err != nil {
-			return
-		}
-		for _, change := range changes.Items {
-			changeCopy := change
-			newLastChange = changeCopy.Id
-			if changeCopy.Id != self.lastChange {
-				result = append(result, changeCopy)
+func (self *dir) mergeChange(change *drive.Change) {
+	childPath := self.Path().add(change.File.Title)
+	if change.File.Labels.Trashed {
+		if _, found := self.childByPath[childPath.String()]; found {
+			for index, child := range self.children {
+				if child.Id() == change.File.Id {
+					self.childrenLock.Lock()
+					newChildren := make([]node, len(self.children)-1)
+					copy(newChildren, self.children[:index])
+					copy(newChildren[index:], self.children[index+1:])
+					self.children = newChildren
+					delete(self.childByPath, childPath.String())
+					self.childrenLock.Unlock()
+					break
+				}
 			}
-		}
-		if changes.NextPageToken != "" {
-			if err = changeLoader(changes.NextPageToken); err != nil {
-				return
-			}
-		}
-		return
-	}
-	if err = changeLoader(""); err != nil {
-		return
-	}
-	self.lastChange = newLastChange
-	return
-}
-
-func (self *futon) cleanChanges() (err error) {
-	changes, err := self.changeList()
-	if err != nil {
-		return
-	}
-	self.folderCacheLock.Lock()
-	defer self.folderCacheLock.Unlock()
-	for _, change := range changes {
-		for _, parent := range change.File.Parents {
-			if d, found := self.folderCacheById[parent.Id]; found {
-				fmt.Println("cleaning", d.Path(), "since", change.File.Title, "was changed")
-				d.clearChildren()
-			}
-		}
-	}
-	return
-}
-
-func (self *futon) acquireToken() (err error) {
-	authUrl := config.AuthCodeURL("state")
-	fmt.Printf("Go to the following link in your browser: %v\n", authUrl)
-	t := &oauth.Transport{
-		Config:    config,
-		Transport: http.DefaultTransport,
-	}
-	fmt.Printf("Enter verification code: ")
-	var code string
-	fmt.Scanln(&code)
-	if self.Token, err = t.Exchange(code); err != nil {
-		return
-	}
-	return
-}
-
-func (self *futon) authorize() (err error) {
-	if err = os.MkdirAll(filepath.Dir(self.auth), 0700); err != nil {
-		return
-	}
-	f, err := os.Open(self.auth)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return
-		}
-		if err = self.acquireToken(); err != nil {
-			return
-		}
-		if f, err = os.Create(self.auth); err != nil {
-			return
-		}
-		defer f.Close()
-		if err = json.NewEncoder(f).Encode(self); err != nil {
-			return
 		}
 	} else {
-		defer f.Close()
-		if err = json.NewDecoder(f).Decode(self); err != nil {
-			return
-		}
+		newChild := self.Futon().newNode(childPath, change.File)
+		self.childrenLock.Lock()
+		self.children = append(self.children, newChild)
+		self.childByPath[childPath.String()] = newChild
+		self.childrenLock.Unlock()
 	}
-	transport := &oauth.Transport{
-		Config:    config,
-		Token:     self.Token,
-		Transport: http.DefaultTransport,
-	}
-	self.client = transport.Client()
-	if self.drive, err = drive.New(self.client); err != nil {
-		return
-	}
-
-	about, err := self.drive.About.Get().Do()
-	if err != nil {
-		return
-	}
-	self.rootFolderId = about.RootFolderId
-	self.lastChange = about.LargestChangeId
-
-	return
-}
-
-func (self *file) getDownloadUrl() (result string, err error) {
-	if self.downloadUrl == "" {
-		var f *drive.File
-		if f, err = self.Futon().drive.Files.Get(self.Id()).Do(); err != nil {
-			return
-		}
-		self.downloadUrl = f.DownloadUrl
-	}
-	result = self.downloadUrl
-	return
-}
-
-func (self *file) read(readReq *fuse.ReadRequest, readResp *fuse.ReadResponse, tries int) (ferr fuse.Error) {
-	if self.Size() == 0 {
-		return
-	}
-	downloadUrl, err := self.getDownloadUrl()
-	if err != nil {
-		return
-	}
-	if downloadUrl == "" {
-		return
-	}
-	req, err := http.NewRequest("GET", downloadUrl, nil)
-	if err != nil {
-		log.Printf("%v", err)
-		ferr = fuse.Error(err)
-		return
-	}
-	req.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", readReq.Offset, readReq.Offset+int64(readReq.Size)-1))
-	resp, err := self.Futon().client.Do(req)
-	if err != nil {
-		log.Printf("%v", err)
-		ferr = fuse.Error(err)
-		return
-	}
-	if resp.StatusCode > 400 && resp.StatusCode < 404 {
-		self.downloadUrl = ""
-		if tries < 5 {
-			return self.read(readReq, readResp, tries+1)
-		}
-	}
-	if resp.StatusCode != 206 {
-		log.Printf("%+v", resp)
-		ferr = fuse.Error(fmt.Errorf("While trying to download %#v: %+v", downloadUrl, resp))
-		return
-	}
-	defer resp.Body.Close()
-	if readResp.Data, err = ioutil.ReadAll(resp.Body); err != nil {
-		log.Printf("%v", err)
-		ferr = fuse.Error(err)
-		return
-	}
-	return
-}
-
-func (self *file) Read(readReq *fuse.ReadRequest, readResp *fuse.ReadResponse, intr fs.Intr) (ferr fuse.Error) {
-	return self.read(readReq, readResp, 0)
-}
-
-func (self *futon) newNode(path path, f *drive.File) (result node) {
-	atime, _ := time.Parse(time.RFC3339, f.LastViewedByMeDate)
-	mtime, _ := time.Parse(time.RFC3339, f.ModifiedDate)
-	base := &nodeImpl{
-		futon: self,
-		path:  path,
-		id:    f.Id,
-		mode:  os.FileMode(0400),
-		size:  f.FileSize,
-		atime: atime,
-		mtime: mtime,
-	}
-	if f.MimeType == "application/vnd.google-apps.folder" {
-		base.mode |= 0100
-		base.mode |= os.ModeDir
-		d := &dir{
-			childByPath: map[string]node{},
-			node:        base,
-		}
-		self.folderCacheLock.Lock()
-		defer self.folderCacheLock.Unlock()
-		self.folderCacheByPath[base.Path().String()] = d
-		self.folderCacheById[base.Id()] = d
-		result = d
-	} else {
-		result = &file{
-			node: base,
-		}
-	}
-	return
-}
-
-type childFetch struct {
-	file *drive.File
-	err  error
 }
 
 func (self *dir) Lookup(name string, intr fs.Intr) (result fs.Node, ferr fuse.Error) {
@@ -387,35 +184,6 @@ func (self *dir) Lookup(name string, intr fs.Intr) (result fs.Node, ferr fuse.Er
 		return
 	}
 	ferr = fuse.Error(fmt.Errorf("No file %#v inside %#v", name, self.Path()))
-	return
-}
-
-func (self *futon) lookup(path path) (result fs.Node, ferr fuse.Error) {
-	self.folderCacheLock.RLock()
-	d, found := self.folderCacheByPath[path.String()]
-	self.folderCacheLock.RUnlock()
-	if found {
-		result = d
-		return
-	}
-	if len(path) == 0 {
-		result = self.newNode(path, &drive.File{
-			Id: self.rootFolderId,
-			UserPermission: &drive.Permission{
-				Type: "owner",
-			},
-			MimeType: "application/vnd.google-apps.folder",
-		})
-		return
-	}
-	parent, err := self.lookup(path[:len(path)-1])
-	if err != nil {
-		return
-	}
-	if d, ok := parent.(*dir); ok {
-		return d.Lookup(path[len(path)-1], make(fs.Intr))
-	}
-	ferr = fuse.Error(fmt.Errorf("No file %#v found"))
 	return
 }
 
@@ -504,6 +272,263 @@ func (self *dir) ReadDir(intr fs.Intr) (result []fuse.Dirent, ferr fuse.Error) {
 		}
 		result[index] = ent
 	}
+	return
+}
+
+// file is a node that has a download url
+type file struct {
+	node
+	downloadUrl string
+}
+
+func (self *file) getDownloadUrl() (result string, err error) {
+	if self.downloadUrl == "" {
+		var f *drive.File
+		if f, err = self.Futon().drive.Files.Get(self.Id()).Do(); err != nil {
+			return
+		}
+		self.downloadUrl = f.DownloadUrl
+	}
+	result = self.downloadUrl
+	return
+}
+
+func (self *file) read(readReq *fuse.ReadRequest, readResp *fuse.ReadResponse, tries int) (ferr fuse.Error) {
+	if self.Size() == 0 {
+		return
+	}
+	downloadUrl, err := self.getDownloadUrl()
+	if err != nil {
+		return
+	}
+	if downloadUrl == "" {
+		return
+	}
+	req, err := http.NewRequest("GET", downloadUrl, nil)
+	if err != nil {
+		log.Printf("%v", err)
+		ferr = fuse.Error(err)
+		return
+	}
+	req.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", readReq.Offset, readReq.Offset+int64(readReq.Size)-1))
+	resp, err := self.Futon().client.Do(req)
+	if err != nil {
+		log.Printf("%v", err)
+		ferr = fuse.Error(err)
+		return
+	}
+	if resp.StatusCode > 400 && resp.StatusCode < 404 {
+		self.downloadUrl = ""
+		if tries < 5 {
+			return self.read(readReq, readResp, tries+1)
+		}
+	}
+	if resp.StatusCode != 206 {
+		log.Printf("%+v", resp)
+		ferr = fuse.Error(fmt.Errorf("While trying to download %#v: %+v", downloadUrl, resp))
+		return
+	}
+	defer resp.Body.Close()
+	if readResp.Data, err = ioutil.ReadAll(resp.Body); err != nil {
+		log.Printf("%v", err)
+		ferr = fuse.Error(err)
+		return
+	}
+	return
+}
+
+func (self *file) Read(readReq *fuse.ReadRequest, readResp *fuse.ReadResponse, intr fs.Intr) (ferr fuse.Error) {
+	return self.read(readReq, readResp, 0)
+}
+
+// futon is saved in $HOME/.futon/auth, but its really just the Token that is saved
+type futon struct {
+	auth         string
+	account      string
+	drive        *drive.Service
+	mountpoint   string
+	rootFolderId string
+	lastChange   int64
+	unmounted    int32
+	conn         *fuse.Conn
+	client       *http.Client
+
+	folderCacheById   map[string]*dir // used when cleaning up folders that are subjected to change
+	folderCacheByPath map[string]*dir // used when finding folders by path
+	folderCacheLock   sync.RWMutex
+
+	Token *oauth.Token
+}
+
+func (self *futon) changeList() (result []*drive.Change, err error) {
+	var changeLoader func(string) error
+	newLastChange := int64(0)
+	changeLoader = func(pageToken string) (err error) {
+		call := self.drive.Changes.List().StartChangeId(self.lastChange).IncludeDeleted(true)
+		if pageToken != "" {
+			call = call.PageToken(pageToken)
+		}
+		changes, err := call.Do()
+		if err != nil {
+			return
+		}
+		for _, change := range changes.Items {
+			changeCopy := change
+			newLastChange = changeCopy.Id
+			if changeCopy.Id != self.lastChange {
+				result = append(result, changeCopy)
+			}
+		}
+		if changes.NextPageToken != "" {
+			if err = changeLoader(changes.NextPageToken); err != nil {
+				return
+			}
+		}
+		return
+	}
+	if err = changeLoader(""); err != nil {
+		return
+	}
+	self.lastChange = newLastChange
+	return
+}
+
+func (self *futon) cleanChanges() (err error) {
+	changes, err := self.changeList()
+	if err != nil {
+		return
+	}
+	self.folderCacheLock.Lock()
+	defer self.folderCacheLock.Unlock()
+	for _, change := range changes {
+		for _, parent := range change.File.Parents {
+			if d, found := self.folderCacheById[parent.Id]; found {
+				d.mergeChange(change)
+			}
+		}
+	}
+	return
+}
+
+func (self *futon) acquireToken() (err error) {
+	authUrl := config.AuthCodeURL("state")
+	fmt.Printf("Go to the following link in your browser: %v\n", authUrl)
+	t := &oauth.Transport{
+		Config:    config,
+		Transport: http.DefaultTransport,
+	}
+	fmt.Printf("Enter verification code: ")
+	var code string
+	fmt.Scanln(&code)
+	if self.Token, err = t.Exchange(code); err != nil {
+		return
+	}
+	return
+}
+
+func (self *futon) authorize() (err error) {
+	if err = os.MkdirAll(filepath.Dir(self.auth), 0700); err != nil {
+		return
+	}
+	f, err := os.Open(self.auth)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+		if err = self.acquireToken(); err != nil {
+			return
+		}
+		if f, err = os.Create(self.auth); err != nil {
+			return
+		}
+		defer f.Close()
+		if err = json.NewEncoder(f).Encode(self); err != nil {
+			return
+		}
+	} else {
+		defer f.Close()
+		if err = json.NewDecoder(f).Decode(self); err != nil {
+			return
+		}
+	}
+	transport := &oauth.Transport{
+		Config:    config,
+		Token:     self.Token,
+		Transport: http.DefaultTransport,
+	}
+	self.client = transport.Client()
+	if self.drive, err = drive.New(self.client); err != nil {
+		return
+	}
+
+	about, err := self.drive.About.Get().Do()
+	if err != nil {
+		return
+	}
+	self.rootFolderId = about.RootFolderId
+	self.lastChange = about.LargestChangeId
+
+	return
+}
+
+func (self *futon) newNode(path path, f *drive.File) (result node) {
+	atime, _ := time.Parse(time.RFC3339, f.LastViewedByMeDate)
+	mtime, _ := time.Parse(time.RFC3339, f.ModifiedDate)
+	base := &nodeImpl{
+		futon: self,
+		path:  path,
+		id:    f.Id,
+		mode:  os.FileMode(0400),
+		size:  f.FileSize,
+		atime: atime,
+		mtime: mtime,
+	}
+	if f.MimeType == "application/vnd.google-apps.folder" {
+		base.mode |= 0100
+		base.mode |= os.ModeDir
+		d := &dir{
+			childByPath: map[string]node{},
+			node:        base,
+		}
+		self.folderCacheLock.Lock()
+		defer self.folderCacheLock.Unlock()
+		self.folderCacheByPath[base.Path().String()] = d
+		self.folderCacheById[base.Id()] = d
+		result = d
+	} else {
+		result = &file{
+			node: base,
+		}
+	}
+	return
+}
+
+func (self *futon) lookup(path path) (result fs.Node, ferr fuse.Error) {
+	self.folderCacheLock.RLock()
+	d, found := self.folderCacheByPath[path.String()]
+	self.folderCacheLock.RUnlock()
+	if found {
+		result = d
+		return
+	}
+	if len(path) == 0 {
+		result = self.newNode(path, &drive.File{
+			Id: self.rootFolderId,
+			UserPermission: &drive.Permission{
+				Type: "owner",
+			},
+			MimeType: "application/vnd.google-apps.folder",
+		})
+		return
+	}
+	parent, err := self.lookup(path[:len(path)-1])
+	if err != nil {
+		return
+	}
+	if d, ok := parent.(*dir); ok {
+		return d.Lookup(path[len(path)-1], make(fs.Intr))
+	}
+	ferr = fuse.Error(fmt.Errorf("No file %#v found"))
 	return
 }
 
